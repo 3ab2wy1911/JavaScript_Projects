e130884e3a057c9660a5892aa5a122be578a85e5
3ab2wy1911 <3ab2wy1911@gmail.com>
2023-06-17 20:16:25 +0300

    Counter
    Simple counter application.

---
+++ Counter/counter.js
@@ -0,0 +1,14 @@
+function decreaseCounter()
+{
+    document.getElementById("screen").innerHTML-=1;
+}
+
+function increaseCounter()
+{
+    document.getElementById("screen").innerHTML-=-1;
+}
+
+function resetCounter()
+{
+    document.getElementById("screen").innerHTML=0;
+}
