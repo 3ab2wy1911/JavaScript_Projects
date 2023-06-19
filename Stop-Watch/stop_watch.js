c97dc130d2d4f87b5566029d7012059f5e0c63e3
3ab2wy1911 <3ab2wy1911@gmail.com>
2023-06-19 11:07:49 +0300

    Update the stop_watch
    start from the milliseconds instead of the seconds.

--- Stop-Watch/stop_watch.js
+++ Stop-Watch/stop_watch.js
@@ -2,9 +2,10 @@ var timer;
 var hr = 0;
 var min = 0;
 var sec = 0;
+var msec = 0;
 
 function startTimer() {
-    timer = setInterval(updateTimer, 1000);
+    timer = setInterval(updateTimer, 10);
 }
 
 function format(time) {
@@ -12,29 +13,31 @@ function format(time) {
     else return time;
 }
 
-
 function updateTimer() {
-    sec++;
-    if (sec === 60) {
-        sec = 0;
-        min++;
-        if (min === 60) {
-            min = 0;
-            hours++;
+    msec++;
+    if (msec === 100) {
+        msec = 0;
+        sec++;
+        if (sec === 60) {
+            sec = 0;
+            min++;
+            if (min === 60) {
+                min = 0;
+                hr++;
+            }
         }
     }
-   document.getElementById("timer").textContent = format(hr) + ":" + format(min) + ":" + format(sec);
+    document.getElementById("timer").textContent = format(hr) + ":" + format(min) + ":" + format(sec) + ":" + format(msec);
 }
 
-function stopTimer()
-{
+function stopTimer() {
     clearInterval(timer);
 }
 
-function resetTimer()
-{
-    document.getElementById("timer").innerHTML = '00:00:00';
+function resetTimer() {
+    document.getElementById("timer").innerHTML = '00:00:00:00';
     hr = 0;
     min = 0;
     sec = 0;
+    msec = 0;
 }
